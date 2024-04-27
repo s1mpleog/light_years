126ef6766bf16b3d3c276359f563dea2db60c058
= <=>
2024-04-27 08:01:13 +0530

    fetched smfl library

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(void) {
+    sf::RenderWindow renderWindow{sf::VideoMode(100, 100), "testing window"};
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
